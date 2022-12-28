6f8ee4c81a8f129626d03fa7f6f85ac32a29b1bb
Colin <colin.jackson@knights.ucf.edu>
2022-12-27 20:01:52 -0500

    first commit

---
+++ automaticEmail.py
@@ -0,0 +1,51 @@
+import imaplib
+
+# Connect to the email server
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+
+# Login to the email account
+mail.login('your_email@example.com', 'your_password')
+
+# Select the inbox
+mail.select('inbox')
+
+# Search for unread emails
+status, emails = mail.search(None, 'UNSEEN')
+
+# Retrieve the first unread email
+status, data = mail.fetch(emails[0], '(RFC822)')
+email_body = data[0][1]
+
+# Generate a response using the OpenAI API and GPT-3 model
+import openai
+openai.api_key = "your_api_key"
+
+response_text = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=f"This is an email: {email_body}\n Write your reply to this email here in a professional manner:\n",
+    temperature=0.5,
+    max_tokens=1024,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+).get('choices')[0].get('text')
+
+# Send the response email
+import email
+from email.mime.text import MIMEText
+
+# Parse the email to get the sender's address
+msg = email.message_from_bytes(email_body)
+sender = msg['From']
+
+# Create the response email
+response = MIMEText(response_text)
+response['To'] = sender
+response['Subject'] = 'RE: ' + msg['Subject']
+
+# Send the email
+mail.sendmail('your_email@example.com', sender, response.as_bytes())
+
+# Close the connection to the email server
+mail.close()
+mail.logout()
