6914b494280b56869b3d42e03dd4a862bbe1f14f
Colin <colin.jackson@knights.ucf.edu>
2022-12-27 20:08:16 -0500

    added unattended version

---
+++ automaticEmail_unattended.py
@@ -0,0 +1,48 @@
+# Abstergo
+
+import win32com.client
+import time
+import openai
+
+# this version will run all day every day
+while True:
+    # Connect to Outlook
+    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+    # Access the inbox
+    inbox = outlook.GetDefaultFolder(6)
+
+    # Search for unread emails
+    unread_emails = inbox.Items.Restrict("[Unread] = true")
+
+    # Retrieve the first unread email
+    email = unread_emails.GetFirst()
+
+    # Check if there are any unread emails
+    if email:
+        # Generate a response using the OpenAI API and GPT-3 model
+        sender = email.Sender.GetExchangeUser().PrimarySmtpAddress
+        
+        openai.api_key = "your_api_key"
+
+        response_text = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"This is an email from {sender}: {email.Body}\n Write your reply to this email here in a professional manner:\n",
+            temperature=0.5,
+            max_tokens=1024,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        ).get('choices')[0].get('text')
+
+        # Send the response email
+        response = email.Reply()
+        response.Body = response_text
+        response.Send()
+
+        # Mark the original email as read
+        email.UnRead = False
+        email.Save()
+
+    # Delay for 1 hour
+    time.sleep(3600)
